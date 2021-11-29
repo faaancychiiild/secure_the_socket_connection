7eb2c52af3b7eaf31521d5e906c983eadb79738e
faaancychiiild <nino.mirziashvili7@gmail.com>
2021-11-29 22:16:40 +0400

    refactor: edit middleware function auth
    to verify tokens

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -8,27 +8,17 @@ const VerifyToken = async (req, res, next) => {
         res.status(403).end();
         return;
     }
-
     let acc = await jwt.verify(req.body.token, process.env.ACCESS_TOKEN_KEY);
 
-    if (!acc) {
+    if (!acc || acc.exp < Date.now() / 1000) {
         //If access token is null, server will verify the refresh token
-        let ref = jwt.verify(req.body.refresh, process.env.REFRESH_TOKEN_KEY);
+        let ref = await jwt.verify(req.body.refresh, process.env.REFRESH_TOKEN_KEY);
         if (!ref) {
             //If refresh token is also null, server will response with 403
             res.status(403).end();
             return;
         }
-        //Else a new access token is generated and request body is modified
-        let token = require('../routes/handlers/gen_tokens').access(user._id.str);
-        req.body.token = token;
-    };
-    if (acc.exp < Date.now() / 1000) {
-        //Handles with expired access tokens
-        let token = require('./gen_tokens').access(user._id.str);
-        req.body.token = token;
     }
-
     next();
 }
 
